b3b0d7823b8f6a224ea22306169d20b2e3c13775
Ashish <ash.ishu025g@gmail.com>
2020-04-08 21:35:43 +0530

    object

--- moduleTest.js
+++ moduleTest.js
@@ -4,4 +4,20 @@ var user = {
     name2: "Linh"
 }
 
-module.exports = user;
+var user2 = new Object({
+    name:"Richard",
+    age: 25,
+    name2: "Linh"
+});
+
+var user3 = {
+    subject:"MERNStack",
+    name:"Himavan"
+}
+
+//user3 = Object.create(user2); //creates an object with prototype chain
+
+//var user3 = Object.create(null); //to break the prototype chain or stop inheritance we use null
+Object.assign(user3, user2);
+
+module.exports = user3;
