3009d6c3600948310dfa8bc4f82bd179f24955f4
Ashish <ash.ishu025g@gmail.com>
2020-04-08 21:10:29 +0530

    module

---
+++ moduleTest.js
@@ -0,0 +1,7 @@
+var user = {
+    name:"Richard",
+    age: 25,
+    name2: "Linh"
+}
+
+module.exports = user;
---
+++ test.js
@@ -0,0 +1,9 @@
+// module
+
+var user = require("./moduleTest");
+
+console.log("User From Module :- ", user);
+
+console.log("User Name :- ", user.name);
+
+console.log("User Name :- ", user.name2);
