22032673b0149bc681bb9ddc9b7a2d188a6e0026
Ashish <ash.ishu025g@gmail.com>
2020-04-20 20:48:22 +0530

    updated stuff

--- moduleTest.js
+++ moduleTest.js
@@ -1,23 +1,42 @@
-
-let promise1 = new Promise(function(resolve, reject)
-{  
-   //  setTimeout(function() {   //async callback 
-   //      //resolve('Resolved Promise');  
-   //      reject({
-   //         "status":400,
-   //         "Error":"Path Not Found"
-   //      });
-   //     }, 300);
-
-   setTimeout(function() {   //async callback               
-       resolve({
-           "status":200,
-           "success":"We are resolved"
-       });
-   }, 400);
-});
-
-
+const heroes = [
+    { name: 'Wolverine',      family: 'Marvel',    isEvil: false },
+    { name: 'Deadpool',       family: 'Marvel',    isEvil: false },
+    { name: 'Magneto',        family: 'Marvel',    isEvil: true  },
+    { name: 'Charles Xavier', family: 'Marvel',    isEvil: false },
+    { name: 'Batman',         family: 'DC Comics', isEvil: false },
+    { name: 'Harley Quinn',   family: 'DC Comics', isEvil: true  },
+    { name: 'Legolas',        family: 'Tolkien',   isEvil: false },
+    { name: 'Gandalf',        family: 'Tolkien',   isEvil: false },
+    { name: 'Saruman',        family: 'Tolkien',   isEvil: true  }
+  ]
+  
+  const newHeroes = heroes.map(h => {
+    h.name = h.name.toUpperCase()
+    return h
+  })
+  
+  //////////
+  const newHeroes = heroes.map(h => {
+    return {
+      ...h,
+      name: h.name.toUpperCase()
+    }
+  })
+  
+  let notEvil = heroes.filter(hero => hero.isEvil == false);  
+  console.log(notEvil);  
+  
+  let uniqueFamilyName = heroes.reduce((prev, curr) => {  
+      prev.add(curr.family);  
+      return prev;  
+  }, new Set());  
+  console.log([...uniqueFamilyName]);
+  
+  function names(obj) {  
+      return obj.map(o => 'Sir ' + o.name);  
+  }  
+  console.log(names(heroes));
+  
 class User{
     constructor(firstName, lastName, age, city){
         this.FirstName = firstName,
--- test.js
+++ test.js
@@ -1,37 +1,8 @@
-//Promise : pending, fullfilled/resolved, rejected
+//Javascript Hacks
+//Some manipulations on below code 
 
+let a = 1;
 
-
-let promise = new Promise((resolve, reject) =>{    
-    //simulating call to server - an async operation
-
-    //an async operation (call to user login)
-        //success /failed/ error
-            //showaccount-callback
-            //logout-callback
-
-    setTimeout(() => {
-        //resolve("any value")
-        resolve({
-            "status":200,
-            "message":"User Signed Successfully",
-            "token":"sdasdasdjashj595923dabjdas"
-        })
-
-        // reject({
-        //     "status":400,
-        //     "message":"User Logged Out Successfully"
-        // })
-    }, 2000);
-
-});
-
-console.log("Status 1 : ", promise); //expected : pending
-
-promise.then((value)=> {  
-    console.log("Then ", value);  
-}).catch((value) => {  
-    console.log("Catch ",value);  
-});
-         
-console.log("Status 2 : ", promise);// expected output: [object Promise]
+if(a==1 && a==2 && a==3){
+    console.log("hello world")
+}
