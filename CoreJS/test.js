d0d556e17cb8658dbdffbb275054f8a6398a5381
Ashish <ash.ishu025g@gmail.com>
2020-04-23 21:12:41 +0530

    push to jsn

--- CoreJS/test.js
+++ CoreJS/test.js
@@ -1,3 +1,21 @@
+let userDetails = [
+{id : 3, name : "Roger", city : "LAX", savedby : "CaptainAmerica" },  
+{id : 4, name : "Adam", city : "BOS", savedby : "IronMan" },  
+{id : 5, name : "Alex", city : "MAA", savedby : "SpiderMan" }  
+];  
+let userDetailsStringified = JSON.stringify(userDetails)  
+console.log("2: AFTER userDetailsStringified : \n\n" + userDetailsStringified + "\n")  
+let fs = require("fs");  
+let writerStream = fs.createWriteStream('./OUT.UserDetails.json')  
+writerStream.write(userDetailsStringified, 'UTF8') // Write the data to stream with encoding to be utf8  
+writerStream.end() // Mark the end of file  
+// Handle stream events --> finish, and error  
+writerStream.on('finish', function() { console.log("3. FINISHED writing (Async). \n") })  
+writerStream.on('error', function(err) { console.log("4. err.stack = ", err.stack, "\n") })  
+console.log("5. END of WriteStream code. \n");  
+// https://jsonformatter.org/
+
+
 // async and sync file read operation
 
 console.log( __filename );
