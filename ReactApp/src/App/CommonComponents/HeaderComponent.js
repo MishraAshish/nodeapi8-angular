0ec093cfe392bf653ec26f6ca7d97e0c029eaae3
Ashish <ash.ishu025g@gmail.com>
2020-05-06 23:16:19 +0530

    more details on jsx

--- ReactApp/src/App/CommonComponents/HeaderComponent.js
+++ ReactApp/src/App/CommonComponents/HeaderComponent.js
@@ -1,18 +1,30 @@
+"use strict";
+
 import React from "react";
 
-export default function Header(params) {
+let Header = (props) => {
     return(
         <div>
             <h1>This is our header component</h1>
+            {props.children[0]}
+            {props.children[1]}
         </div>
     )
 }
-
+export default Header;
 
 export function GetName() {    
     return(//JSX
         <div>
             <b>Get Name Function Called</b>
+        </div>
+    )    
+}
+
+export function GetAge() {    
+    return(//JSX
+        <div>
+            <b>GetAge Function Called</b>
         </div>
     )    
 }
--- ReactApp/src/App/app.js
+++ ReactApp/src/App/app.js
@@ -1,21 +1,31 @@
 import React        from "react";
 import {Component, useState }  from "react";
-import Header,{GetName} from "./CommonComponents/HeaderComponent";
+import Header, * as all from "./CommonComponents/HeaderComponent";
 
-//export class App extends React.Component{
-export default class App extends Component{
+export default class App extends React.Component{
+//export default class App extends Component{
     render(){//virtual dom
         let a = 25, b = 26, framework = "React", version = "16.4";
         let title = `We are going to learn ${framework} Framework with version ${version}`;
+        let names = ["Andrew","Himavan","Richard","Linh"];
         return(//JSX
             <div>
                 <b>This should load from react page</b>
                 <p>{'Addition '+ a + b}{'Multi - '+ a * b}</p>
                 <div>
                     <b>{title}</b>
+                    <ul>{
+                        names.filter((item)=>{
+                            return <li>{item}</li>
+                        })
+                    }</ul>
                 </div>
-                <Header />
-                <GetName />
+                <Header>
+                    <all.GetName />    
+                    <all.GetAge />
+                </Header>
+                
+                
             </div>
         )
     }
