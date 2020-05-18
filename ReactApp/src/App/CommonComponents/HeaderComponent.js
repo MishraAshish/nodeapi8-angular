2292ac2b7548061a34a5d35de33cc786f92f4970
Ashish <ash.ishu025g@gmail.com>
2020-05-18 22:35:09 +0530

    subscribed comp

--- ReactApp/src/App/CommonComponents/HeaderComponent.js
+++ ReactApp/src/App/CommonComponents/HeaderComponent.js
@@ -1,22 +1,39 @@
 "use strict";
-
+//{props.user.userName}
 import React from "react";
+import {connect} from "react-redux";
 import {NavLink} from "react-router-dom";
+//import {action} from "action/"
 
 let Header = (props) => {
     //read user from props his name and then create two set of links
     return(
         <div className="col-md-12">
+            <div>
+            <b>Hi {props.user.userName}, Welcome to Synergisticit Shopping Cart App</b>
+            </div>
              <React.Fragment>
                  <NavLink to="/home" exact className="button" activeClassName="success" >Home </NavLink> 
                  <NavLink to="/user" exact className="button" activeStyle={{ color:'white'}} activeClassName="success" >Login  </NavLink>
                  <NavLink to="/about" exact className="button" activeClassName="success">About  </NavLink>
              </React.Fragment>
          </div>
- )
+    )
 }
-export default Header;
 
+let mapStateToProps = (state)=>{
+    return {
+        user:state.user.user
+    }
+}
+
+// let mapDispatchToProps = (dispatch)=>{
+//     return {
+//         user:state.user.user
+//     }
+// }
+
+export default connect(mapStateToProps, null)(Header);
 // export function GetName() {    
 //     return(//JSX
 //         <div>
--- ReactApp/src/App/State/UserReducer.js
+++ ReactApp/src/App/State/UserReducer.js
@@ -22,6 +22,10 @@ export default function userReducer(state=INITIAL_STATE, action) {
             console.log("User PayLoad", action.payload.user);
                 //below code is maintaining the immutability of state using spread operator
                 //...state = {user:user,laoding:loading}
+                //{loading:loading} && user = action.payload.user (new user object gets created)
+                
+                //{loading:loading} + {user:user} = {loading:loading, user:user} - a new state object is returned
+
                 //it will pluck user obeject and update that with payload.user value
                 //then a complete new state would be returned from user reducer
                 return {...state, user: action.payload.user}
