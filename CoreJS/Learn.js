5046c43d44976e90aae7b331ef1f38527b32d595
Ashish <ash.ishu025g@gmail.com>
2020-06-30 20:15:38 +0530

    socket server

--- CoreJS/Learn.js
+++ CoreJS/Learn.js
@@ -1,13 +1,24 @@
-//require.extensions;
-// NPM and 
-console.log(require.extensions);
+//creating a tcp server
 
-setImmediate(()=>{
-  console.log("This is ashish set immediate. ");
-  
+const server = require("net").createServer();
+
+server.on("connection", socket => {
+  console.log("socket", socket);
+  socket.write("Welcome new client! \n")
 })
 
+server.listen(8000, ()=>{"Server Bound! "})
+
+//require.extensions;
+// NPM and 
+//console.log(require.extensions);
+
+// setImmediate(()=>{
+//   console.log("This is ashish set immediate. ");
+  
+// })
+
 //console.log(process.env);
-console.log(process.argv);
+//console.log(process.argv);
 
 //libuv : c library developed for node now used in other libraries as well, used abstract non-blocking io, tcp udp scokcets, includes threadpool to work asynchronously
