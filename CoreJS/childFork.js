75f99090ae76088d1d74afbfb9b46220720a0a26
Ashish <ash.ishu025g@gmail.com>
2020-07-07 20:34:55 +0530

    fork

---
+++ CoreJS/childFork.js
@@ -0,0 +1,11 @@
+const { log } = require("console")
+
+process.on("message",(msg)=>{
+    console.log("Message from Parent :", msg);    
+})
+
+let counter = 0;
+
+setInterval(() => {
+    process.send({counter : counter++})
+}, 1000);
